4f1de5ef1da8ebb5b16141fe1de29f04dd19a657
Ihor Lukyanenko <gary.lukyanenko@gmail.com>
2015-09-05 17:03:06 +0200

    Moved beanconfig.xml to a dedicated package Removed the potential leak of resources in the main method

--- basicspringapp/src/main/java/be/il2consulting/basicspringapp/Application.java
+++ basicspringapp/src/main/java/be/il2consulting/basicspringapp/Application.java
@@ -1,18 +1,17 @@
 package be.il2consulting.basicspringapp;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Application {
 
-	private static ApplicationContext ac;
-
 	public static void main(String[] args) {
 
-		ac = new FileSystemXmlApplicationContext("beanconfig.xml");
+		FileSystemXmlApplicationContext ac = new FileSystemXmlApplicationContext("src/main/java/be/il2consulting/beans/beanconfig.xml");
 		
 		Car car = (Car)ac.getBean("car");
 		car.drive();
+		
+		ac.close();
 		
 	}
 
